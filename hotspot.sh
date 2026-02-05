ac37740f2c9e67aacd26c1221f69c653d4b165a0
slmeddah <125124447+slmeddah@users.noreply.github.com>
2026-02-05 19:57:20 +0100

    Delete hotspot.sh

--- hotspot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo ifconfig wlan0 192.168.20.18
-sudo systemctl restart dnsmasq
-sudo sysctl net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo hostapd /etc/hostapd/hostapd.conf
